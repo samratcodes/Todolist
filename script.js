ba08d5164ed035fee1891cdcee7db6cc54de0c44
Samir Nepane <sasuki984@gmail.com>
2023-05-22 09:42:18 +0545

    added some functionality

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
-const input=document.getElementById('input')
-const form= document.querySelector('form')
-const ul =document.querySelector('#List')
-function List(input){
-    const workList=document.createElement('li')
-    workList.className="list-group-item"
-    workList.innerHTML=`<div d-flex justify-content-spacebetween>
+const input = document.getElementById("input");
+const form = document.querySelector("form");
+const ul = document.querySelector("#List");
+function List(input) {
+  const workList = document.createElement("li");
+  workList.className = "list-group-item";
+  workList.innerHTML = `<div d-flex justify-content-spacebetween>
   <div class="input-group mb-3">
   <div class="input-group-text">
     <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
   </div>
-  <h4>${input}</h4>
+  <input class="list-item-context" value=${input} readonly style="border:none; outline:none; margin-left:16px;"/>
 </div> 
 <div>
-<button type="submit">Edit</button>
+<button type="submit" class="edit-btn">Edit</button>
 <button class='delete-btn' type="submit">Delete</button>
 
 </div>
 </div>
-  `
-    ul.append(workList)
- const deleteButton = workList.querySelector('.delete-btn');
-    deleteButton.addEventListener('click', () => {
-      workList.remove();
-    });
+  `;
+  ul.append(workList);
+  const deleteButton = workList.querySelector(".delete-btn");
+  const editButton = workList.querySelector(".edit-btn");
+  const checkButton = workList.querySelector(".form-check-input");
+
+  //detete btn action
+  deleteButton.addEventListener("click", () => {
+    workList.remove();
+  });
+
+  //edit btn action
+  editButton.addEventListener("click", () => {
+    workList.querySelector(".list-item-context").toggleAttribute("readonly");
+    workList.querySelector(".list-item-context").focus();
+  });
+
+  //check btn action
+  checkButton.addEventListener("click", () => {
+    const listItem = workList.querySelector(".list-item-context");
+
+    if (listItem.style.textDecoration === "") {
+      listItem.style.textDecoration = "line-through";
+    } else {
+      listItem.style.textDecoration = "";
+    }
+  });
 }
 
-
-form.addEventListener('submit',(event) => {
-    event.preventDefault()
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //check case for empty empty input field
+  if (input.value === "") {
+    return;
+  } else {
     List(input.value);
-
-})
+    // clearing input field after submit button click
+    input.value = "";
+  }
+});
